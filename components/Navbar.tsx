6499306c0333d58e96c26eb2c6bc7f653a042b0e
Naufal Romero <88795011+romves@users.noreply.github.com>
2023-10-11 00:11:24 +0700

    change navbutton size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -82,13 +82,13 @@ const Navbar = () => {
             ) : (
               <div className="w-7 h-7 flex flex-col justify-between items-center lg:hidden text-4xl cursor-pointer overflow-hidden group">
                 <span
-                  className={`w-full h-[3px] bg-black inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300  `}
+                  className={`w-full h-[4px] bg-black inline-flex transform group-hover:translate-x-2 transition-all ease-in-out duration-300  `}
                 ></span>
                 <span
-                  className={`w-full h-[3px] bg-black inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300 `}
+                  className={`w-full h-[4px] bg-black inline-flex transform translate-x-3 group-hover:translate-x-0 transition-all ease-in-out duration-300 `}
                 ></span>
                 <span
-                  className={`w-full h-[3px] bg-black inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300 `}
+                  className={`w-full h-[4px] bg-black inline-flex transform translate-x-1 group-hover:translate-x-3 transition-all ease-in-out duration-300 `}
                 ></span>
               </div>
             )}
